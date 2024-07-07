d27a1b2f06a6d39adb13c6cda10ca228f386b8a8
Santiago DellG7 <santiago.penagos@uni-jena.de>
2024-07-07 20:41:38 +0200

    updates main files

---
+++ test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib
+# matplotlib.use("QtAgg")
+matplotlib.use("agg")
+# matplotlib.use("nbAgg")
+import matplotlib.pyplot as plt
+
+# matplotlib.use("WebAgg")
+
+"""
+Load image as sample
+"""
+# Load an image and normalize it
+my_object_RGB = plt.imread('imgs/PiotrZakrzewski_5197202.png')  # RGB image
+fig, ax = plt.subplots()
+ax.imshow(my_object_RGB)
+fig.show()
+# plt.show()
