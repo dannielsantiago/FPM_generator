5587d8d4e1b8a73e133243e15fd9e8f4be4f1afe
Santiago DellG7 <santiago.penagos@uni-jena.de>
2025-05-04 20:04:07 +0200

    adjust reconstruction

--- FPM_reconstruction.py
+++ FPM_reconstruction.py
@@ -1,3 +1,5 @@
+import h5py
+import numpy as np
 import matplotlib
 try:
     matplotlib.use("tkagg")
@@ -6,19 +8,30 @@ except:
 import PtyLab
 from PtyLab import Engines
 import logging
-
+from PtyLab.utils.utils import fft2c, ifft2c
 logging.basicConfig(level=logging.INFO)
 
 
-filePath = "datasets/2025_04_06/my_FPM_dataset.h5"
+filePath = "datasets/2025_05_04/my_FPM_dataset.h5"
 
 experimentalData, reconstruction, params, monitor, engine, calib = PtyLab.easyInitialize(
     filePath, operationMode="FPM"
 )
 
 experimentalData.setOrientation(0)
+###entrance pupil diamerer
+experimentalData.NA=None
+# experimentalData.entrancePupilDiameter=20
+experimentalData._setData()
 reconstruction.copyAttributesFromExperiment(experimentalData)
 reconstruction.computeParameters()
+
+with h5py.File(filePath, 'r') as hf:
+    positions = hf.get('encoder_px')[()]
+
+# reconstruction.positions0=-1*positions+reconstruction.No//2
+
+
 # %% Prepare everything for the reconstruction
 # now, all our experimental data is loaded into experimental_data and we don't have to worry about it anymore.
 # now create an object to hold everything we're eventually interested in
@@ -30,26 +43,28 @@ calib.plot = True
 calib.fit_mode ='SimilarityTransform'
 calib.calibrateRadius = True
 calib.fit_mode = "Translation"
-calib.runCalibration()
+# calib.runCalibration()
 
 # %% Prepare reconstruction post-calibration
 reconstruction.initializeObjectProbe()
+
 
 # %% Set monitor properties
 monitor.figureUpdateFrequency = 1
 monitor.objectPlot = "complex"  # complex abs angle
 monitor.verboseLevel = "low"  # high: plot two figures, low: plot only one figure
-monitor.objectZoom = 0.01  # control object plot FoVW
-monitor.probeZoom = 0.01  # control probe plot FoV
+monitor.objectZoom = 0.1  # control object plot FoVW
+monitor.probeZoom = 0.1  # control probe plot FoV
 
 # %% Set param
 params.gpuSwitch = True
-params.positionOrder = "NA"
+params.positionOrder = "random"
 params.probePowerCorrectionSwitch = False
 params.comStabilizationSwitch = False
 params.probeBoundary = True
 params.adaptiveDenoisingSwitch = True
-# Params.positionCorrectionSwitch = True
+params.propagator = 'Fourier'
+# params.positionCorrectionSwitch = False
 # Params.backgroundModeSwitch = True
 
 #%% Run the reconstructors
